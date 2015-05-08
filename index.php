9b477bc9102e11a124ddaf77b4030841b8a6c581
sebas932 <sebas932@gmail.com>
2015-05-08 16:03:41 -0500

    Improved index.php

--- index.php
+++ index.php
@@ -1,27 +1,36 @@
 <?php
 require_once('vendor/autoload.php');
-use Scriptotek\OaiPmh\Client as OaiPmhClient;
+use Scriptotek\OaiPmh\Client as OaiPmhClient;  
+ 
+//$recordId = 'oai:cgspace.cgiar.org:10568/52163'; 
 
-$url = 'https://thedata.harvard.edu/dvn/OAIHandler';
+getCGSpace('oai:cgspace.cgiar.org:10568/52163');
 
-$client = new OaiPmhClient($url, array(
-    'schema' => 'marcxchange',
-    'user-agent' => 'MyTool/0.1',
-    'max-retries' => 10,
-    'sleep-time-on-error' => 30,
-));
-
-
-try {
-    $record = $client->record('hdl:1902.1/22584');
-} catch (Scriptotek\OaiPmh\ConnectionError $e) {
-    echo $e->getMsg();
-    die;
-} catch (Scriptotek\OaiPmh\BadRequestError $e) {
-    echo 'Bad request: ' . $e->getMsg() . "\n";
-    die;
+function getCGSpace($recordId){ 
+	$url = 'https://cgspace.cgiar.org/oai/request';
+	echo getMetadata($url, $recordId);
 }
 
-echo $record->identifier . "\n";
-echo $record->datestamp . "\n";
-echo $record->data->asXML() . "\n";
+
+function getMetadata($url, $recordId){  
+	$client= new OaiPmhClient($url, array(
+    'schema' => 'oai_dc',
+    'user-agent' => 'DataTools/0.1',
+    'max-retries' => 10,
+    'sleep-time-on-error' => 30,
+	));
+
+	try {
+    $record = $client->record($recordId); 
+	} catch (Scriptotek\OaiPmh\ConnectionError $e) {
+	    echo $e->getMsg();
+	    die;
+	} catch (Scriptotek\OaiPmh\BadRequestError $e) {
+	    echo 'Bad request: ' . $e->getMsg() . "\n";
+	    die;
+	}
+
+	//return $record->datestamp . "\n";
+	return $record->data->asXML();
+
+}
