1058166b482a0f8e05ddb1d41f95276faf9f59d8
sebas932 <sebas932@gmail.com>
2015-05-12 13:25:28 -0500

    Getting the metadata in JSON format

--- index.php
+++ index.php
@@ -7,12 +7,15 @@ use Scriptotek\OaiPmh\Client as OaiPmhClient;
 getCGSpace('oai:cgspace.cgiar.org:10568/52163');
 
 function getCGSpace($recordId){ 
-	$url = 'https://cgspace.cgiar.org/oai/request';
-	echo getMetadata($url, $recordId);
+	$url = 'https://cgspace.cgiar.org/oai/request';   
+	$node = requestMetadata($url, $recordId)->first('oai_dc:dc');  
+
+	$json = json_encode(getMetadata($node));
+	echo ($json);
 }
 
 
-function getMetadata($url, $recordId){  
+function requestMetadata($url, $recordId){  
 	$client= new OaiPmhClient($url, array(
     'schema' => 'oai_dc',
     'user-agent' => 'DataTools/0.1',
@@ -31,6 +34,75 @@ function getMetadata($url, $recordId){
 	}
 
 	//return $record->datestamp . "\n";
-	return $record->data->asXML();
+	return $record->data;
 
 }
+
+
+function getMetadata ($node){
+
+	$output = array();
+	// Title
+	foreach ($node->all('dc:title') as $item) {
+   	$output["title"][] = (string)$item;
+	}
+	// Creator
+	foreach ($node->all('dc:creator') as $item) {
+   	$output["creator"][] = (string)$item;
+	}
+	// Subject
+	foreach ($node->all('dc:subject') as $item) {
+   	$output["subject"][] = (string)$item;
+	}
+	// Description
+	foreach ($node->all('dc:description') as $item) {
+   	$output["description"][] = (string)$item;
+	}
+	// Publisher
+	foreach ($node->all('dc:publisher') as $item) {
+   	$output["publisher"][] = (string)$item;
+	} 
+	// Contributor
+	foreach ($node->all('dc:contributor') as $item) {
+   	$output["contributor"][] = (string)$item;
+	}
+	// Date
+	foreach ($node->all('dc:date') as $item) {
+   	$output["date"][] = (string)$item;
+	}
+	// Type
+	foreach ($node->all('dc:type') as $item) {
+   	$output["type"][] = (string)$item;
+	}
+	// Format
+	foreach ($node->all('dc:format') as $item) {
+   	$output["format"][] = (string)$item;
+	}
+	// Identifiers
+	foreach ($node->all('dc:identifier') as $item) {
+   	$output["identifier"][] = (string)$item;
+	}
+	// Source
+	foreach ($node->all('dc:source') as $item) {
+   	$output["source"][] = (string)$item;
+	}
+	// Language
+	foreach ($node->all('dc:language') as $item) {
+   	$output["language"][] = (string)$item;
+	}
+	// Relation
+	foreach ($node->all('dc:relation') as $item) {
+   	$output["relation"][] = (string)$item;
+	}
+	// Coverage
+	foreach ($node->all('dc:coverage') as $item) {
+   	$output["coverage"][] = (string)$item;
+	}
+	// Rights
+	foreach ($node->all('dc:rights') as $item) {
+   	$output["rights"][] = (string)$item;
+	} 
+
+	return $output;
+}
+
